293d06cfff7c2c4717be3eba36b5854c5409bc19
Yahya Mammadli <yahya.mammadli07@gmail.com>
2026-03-19 23:36:55 +0400

    Add files via upload

---
+++ DLL/Models/Currency.cs
@@ -0,0 +1,50 @@
+﻿
+
+namespace ConvertingApp.Converter.Models;
+
+public class Currency
+{
+    static double AZN = 1.7;
+    static double USD = 1;
+    static double EUR = 0.87;
+
+
+    public static double Convert(double amount, string from, string to)
+    {
+        var fromRate = GetRate(from);
+
+        var toRate = GetRate(to);
+
+        var amountInUsd = amount / fromRate;
+
+        return amountInUsd* toRate;
+
+
+
+
+    }
+
+
+
+    private static double GetRate(string currency)
+    {
+
+        switch (currency.ToUpper())
+        {
+
+            case "AZN": 
+                return AZN;
+            case "USD":
+                return USD;
+            case "EUR":
+                return EUR;
+            default: throw new Exception($"Unknown currency => {currency}\n");
+                
+        }
+
+    }
+     
+
+
+
+}
---
+++ DLL/Program.cs
@@ -0,0 +1,44 @@
+﻿using ConvertingApp.Converter.Models;
+
+
+
+
+namespace ConvertingApp.ConsoleApp;
+
+public class Program
+{
+
+    static void Converter(double amount, string from, string to)
+    {
+        var result = Currency.Convert(amount, from, to);
+
+        Console.Write($"\n\nResult => {result} {to}\n\n");
+
+
+
+    }
+
+
+    static void Main()
+    {
+        Console.Write("Enter amount => ");
+        var amount = int.Parse(Console.ReadLine()!);
+
+        Console.Write("\n\nEnter current currency (AZN, USD, EUR) => ");
+        var from = Console.ReadLine()!;
+
+        Console.Write("\n\nEnter currency you want to convert to (AZN, USD, EUR) => ");
+        var to = Console.ReadLine()!;
+
+
+
+        Converter(amount, from, to);
+
+
+
+
+
+    }
+
+
+}
